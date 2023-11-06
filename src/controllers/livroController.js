e5d8c8ffc13ebbb35f1bc79caaba36fcf915006c
Marcos Lopes <marcospaulolopesc@gmail.com>
2023-11-06 14:08:47 -0300

    fim do curso

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
   static async listarLivros(req, res) {
@@ -21,9 +22,12 @@ class LivroController {
   }
 
   static async cadastrarLivro(req, res) {
+    const novoLivro = req.body;
     try {
-      const novoLivro = await livro.create(req.body);
-      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+      const livroCriado = await livro.create(livroCompleto);
+      res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
     }
@@ -46,6 +50,16 @@ class LivroController {
       res.status(200).json({ message: "livro excluído com sucesso" });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    }
+  }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na busca` });
     }
   }
 }
